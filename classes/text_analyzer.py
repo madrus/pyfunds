13dbb49672c0d5af748779daf04a5b41ac9a2992
madrus <madrus@gmail.com>
2016-05-07 07:37:21 +0200

    we return tuple but one of the old tests is now broken

--- classes/text_analyzer.py
+++ classes/text_analyzer.py
@@ -12,10 +12,16 @@ def analyze_text(filename):
         IOError: If `filename` does not exist or can't be read.
 
     Returns:
-        The number of lines in the file.
+        A tuple where the first element is the number of lines in the file
+        and the second element is the number of characters in it.
     """
+    lines = 0
+    chars = 0
     with open(filename, 'r') as f:
-        return sum(1 for _ in f)
+        for line in f:
+            lines += 1
+            chars += len(line)
+        return lines, chars
 
 
 class TextAnalysisTests(unittest.TestCase):
