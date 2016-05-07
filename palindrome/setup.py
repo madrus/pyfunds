16dab217178f15defc30b7f0128cced7e3e381e5
madrus <madrus@gmail.com>
2016-05-07 13:53:42 +0200

    added virtual environment for setup and deployment of palindrom project

---
+++ palindrome/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='palindrome',
+    version='1.0',
+    py_modules=['palindrome'],
+
+    # metadata
+    author='Andre Roussakoff',
+    author_email='madrus@gmail.com',
+    description='A module for finding palindromatic integers.',
+    license='Public domain',
+    keywords='example, palindrome',
+    )
